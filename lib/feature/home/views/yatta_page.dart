0d56467dcd28e0a4d8e567fe13237869ac592561
GamingDev27 <miko.gaming1975@gmail.com>
2025-08-12 22:18:24 +0800

    field,checkbox,buttons,dropdown

--- lib/feature/home/views/yatta_page.dart
+++ lib/feature/home/views/yatta_page.dart
@@ -1,10 +1,130 @@
 import 'package:flutter/material.dart';
 
-class YattaPage extends StatelessWidget {
+class YattaPage extends StatefulWidget {
   const YattaPage({super.key});
 
   @override
+  State<YattaPage> createState() => _YattaPageState();
+}
+
+class _YattaPageState extends State<YattaPage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
+  String? menuItem = '1';
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Yatta Page'));
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DropdownButton(
+              value: menuItem,
+              items: [
+                DropdownMenuItem(value: '1', child: Text('Monday')),
+                DropdownMenuItem(value: '2', child: Text('Wednesday')),
+                DropdownMenuItem(value: '3', child: Text('Friday')),
+                DropdownMenuItem(value: '4', child: Text('Saturday')),
+              ],
+              onChanged: (String? value) {
+                setState(() {
+                  menuItem = value;
+                });
+              },
+            ),
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(controller.text),
+            Checkbox(
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile(
+              value: isChecked,
+              title: Text('Hello'),
+              onChanged: (value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: Text('Run on Background'),
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider(
+              value: sliderValue,
+              max: 10,
+              divisions: 10,
+              onChanged: (value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+            ),
+            Text(sliderValue.toString()),
+            GestureDetector(
+              onTap: () {
+                print('first image');
+              },
+              child: Image.asset('assets/images/suguru-geto.jpeg', height: 200),
+            ),
+            InkWell(
+              splashColor: Colors.white10,
+              onTap: () {
+                print('second image');
+              },
+              child: Image.asset('assets/images/suguru-geto.jpeg', height: 200),
+            ),
+            Image.asset('assets/images/suguru-geto.jpeg', height: 200),
+            Image.asset('assets/images/suguru-geto.jpeg', height: 200),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {},
+                  child: Text('Save'),
+                ),
+                ElevatedButton(onPressed: () {}, child: Text('Edit')),
+                FilledButton(onPressed: () {}, child: Text('Exit')),
+                TextButton(onPressed: () {}, child: Text('Skip')),
+                CloseButton(onPressed: () {}),
+                BackButton(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
