1796446069b73a4f49fd61c9fc3174be9302aa95
GamingDev27 <miko.gaming1975@gmail.com>
2025-08-12 10:45:48 +0800

    Flutter Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -25,48 +25,72 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            title1!,
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.indigo,
-        ),
-        drawer: SafeArea(
-          child: Drawer(
-            child: Column(children: [ListTile(title: Text('logout'))]),
-          ),
-        ),
-        floatingActionButton: SizedBox(
-          width: 80,
-          height: 80,
-          child: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.greenAccent,
-            child: Icon(Icons.qr_code),
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      home: MyHomePage(),
+    );
+  }
+}
 
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.cached), label: "Yatta!"),
-            NavigationDestination(icon: Icon(Icons.menu), label: "Menu"),
-            NavigationDestination(icon: Icon(Icons.search), label: "Search"),
-            NavigationDestination(
-              icon: Icon(Icons.settings),
-              label: "Settings",
-            ),
-          ],
-          onDestinationSelected: (int value) {
-            print(value);
-          },
-          backgroundColor: Colors.indigo,
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title1!,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+      drawer: SafeArea(
+        child: Drawer(
+          child: Column(children: [ListTile(title: Text('logout'))]),
+        ),
+      ),
+      floatingActionButton: SizedBox(
+        width: 80,
+        height: 80,
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.greenAccent,
+          child: Icon(Icons.qr_code),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: currentIndex == 0
+          ? Center(child: Text('home'))
+          : currentIndex == 1
+          ? Center(child: Text('Yatta!'))
+          : currentIndex == 2
+          ? Center(child: Text('Menu'))
+          : currentIndex == 3
+          ? Center(child: Text('Search'))
+          : Center(child: Text('Settings')),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.cached), label: "Yatta!"),
+          NavigationDestination(icon: Icon(Icons.menu), label: "Menu"),
+          NavigationDestination(icon: Icon(Icons.search), label: "Search"),
+          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
+        ],
+        onDestinationSelected: (int value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        backgroundColor: Colors.indigo,
+        selectedIndex: currentIndex,
       ),
     );
   }
 }
+
