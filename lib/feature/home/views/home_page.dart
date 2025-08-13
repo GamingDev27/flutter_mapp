db3056f9caa6d5be32df73fea5bd8c81d44ce4be
TheCorvinus <markus.corvinus0827@gmail.com>
2025-08-13 23:31:42 +0800

    Divider,Card,Text Constant

--- lib/feature/home/views/home_page.dart
+++ lib/feature/home/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mapp/data/constant.dart';
 import 'package:flutter_mapp/widget/hero.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,7 +9,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(children: [HeroWidget()]),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Trip Today', style: KTextStyle.titleTextTeal),
+                    Text('24', style: KTextStyle.descriptionTextTeal),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
